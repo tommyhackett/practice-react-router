62694a2b8b80e9bb49ee378ca49d43a42e2dd888
tommyhackett <tommyhackett22@gmail.com>
2022-09-12 16:07:05 +0100

    Test deploying with router app

---
+++ src/components/Courses.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Route} from "react-router-dom";
+import {NavLink} from "react-router-dom";
+import {Redirect} from "react-router-dom";
+import CourseContainer from "./courses/CourseContainer";
+import { CSSCourses, HTMLCourses, JSCourses } from '../data/courses';
+
+const Courses = ({match}) => (
+  <div className="main-content courses">
+    <div className="course-header group">
+      <h2>Courses</h2>
+      <ul className="course-nav">
+        <li><NavLink to={`${match.url}/html`}>HTML</NavLink></li>
+        <li><NavLink to={`${match.url}/css`}>CSS</NavLink></li>
+        <li><NavLink to={`${match.url}/javascript`}>JavaScript</NavLink></li>
+      </ul>
+    </div>
+
+    <Route exact path={match.path} render={ () => <Redirect to={`${match.path}/html`} /> }/>
+
+    <Route path={`${match.path}/html`}
+           render={() => <CourseContainer data={HTMLCourses} /> } />
+    <Route path={`${match.path}/css`}
+        render={() => <CourseContainer data={CSSCourses} /> } />
+    <Route path={`${match.path}/javascript`}
+           render={() => <CourseContainer data={JSCourses} /> } />
+  </div>
+);
+
+export default Courses;
